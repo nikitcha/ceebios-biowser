ad72e6e7f88a7400199bfa65b5d747369c8d3cdb
Nikolay Tchakarov <nikitcha@yahoo.com>
2021-06-14 15:34:22 +0200

    deploy

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import dash
 import dash_bootstrap_components as dbc
+import flask
 
-app = dash.Dash(__name__, title='Ceebios Biowser', suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
+server = flask.Flask(__name__) # define flask app.server
+
+app = dash.Dash(__name__, 
+                title='Ceebios Biowser', 
+                suppress_callback_exceptions=True, 
+                external_stylesheets=[dbc.themes.BOOTSTRAP],
+                server=server)
+
